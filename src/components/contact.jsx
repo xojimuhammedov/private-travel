71603a4b35eaf5c55c3afd0ddee1f429cb870a2b
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-07-11 14:20:30 +0500

    fixed bugs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,6 +5,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 import Footer from "./footer";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
+import { Fade } from "react-reveal";
 export default function Contact() {
   const { t } = useTranslation();
 
@@ -75,31 +76,33 @@ export default function Contact() {
               </a>
             </div>
           </div>
-          <div className="more-reservation">
-            <div className="row-reservation">
-              <div className="info-item">
-                <i>
-                  <MdOutlineLocalPhone />
-                </i>
-                <h4>{t("phone")}</h4>
-                <p>+998990621736</p>
-              </div>
-              <div className="info-item">
-                <i>
-                  <CgMail />
-                </i>
-                <h4>{t("email")}</h4>
-                <p>+998990621736</p>
-              </div>
-              <div className="info-item">
-                <i>
-                  <FaLocationArrow />
-                </i>
-                <h4>{t("office")}</h4>
-                <p>Tashkent</p>
+          <Fade bottom>
+            <div className="more-reservation">
+              <div className="row-reservation">
+                <div className="info-item">
+                  <i>
+                    <MdOutlineLocalPhone />
+                  </i>
+                  <h4>{t("phone")}</h4>
+                  <p>+998990621736</p>
+                </div>
+                <div className="info-item">
+                  <i>
+                    <CgMail />
+                  </i>
+                  <h4>{t("email")}</h4>
+                  <p>+998990621736</p>
+                </div>
+                <div className="info-item">
+                  <i>
+                    <FaLocationArrow />
+                  </i>
+                  <h4>{t("office")}</h4>
+                  <p>Tashkent</p>
+                </div>
               </div>
             </div>
-          </div>
+          </Fade>
           <div className="map">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d196581.27772823905!2d66.9515776!3d39.659110399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1719583699981!5m2!1sen!2s"
@@ -108,41 +111,43 @@ export default function Contact() {
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
-          <div className="connect">
-            <h1>{t("joy")}</h1>
-            <div className="row-connect">
-              <div className="cold">
-                <fieldset>
-                  <label htmlFor="">{t("ism")}</label> <br />
-                  <input
-                    value={nameValue}
-                    onChange={(e) => changeName(e.target.value)}
-                    type="text"
-                  />
-                </fieldset>
+          <Fade bottom>
+            <div className="connect">
+              <h1>{t("joy")}</h1>
+              <div className="row-connect">
+                <div className="cold">
+                  <fieldset>
+                    <label htmlFor="">{t("ism")}</label> <br />
+                    <input
+                      value={nameValue}
+                      onChange={(e) => changeName(e.target.value)}
+                      type="text"
+                    />
+                  </fieldset>
+                </div>
+                <div className="cold">
+                  <fieldset>
+                    <label htmlFor="">{t("telefon")}</label>
+                    <input
+                      value={numberValue}
+                      onChange={(e) => changeNumber(e.target.value)}
+                      type="number"
+                    />
+                  </fieldset>
+                </div>
+                <div className="cold-1">
+                  <fieldset>
+                    <label htmlFor="">{t("Message")}</label>
+                    <textarea
+                      value={textValue}
+                      onChange={(e) => changeText(e.target.value)}
+                    />
+                  </fieldset>
+                </div>
               </div>
-              <div className="cold">
-                <fieldset>
-                  <label htmlFor="">{t("telefon")}</label>
-                  <input
-                    value={numberValue}
-                    onChange={(e) => changeNumber(e.target.value)}
-                    type="number"
-                  />
-                </fieldset>
-              </div>
-              <div className="cold-1">
-                <fieldset>
-                  <label htmlFor="">{t("Message")}</label>
-                  <textarea
-                    value={textValue}
-                    onChange={(e) => changeText(e.target.value)}
-                  />
-                </fieldset>
-              </div>
+              <button onClick={sendMessage}>{t("making")}</button>
             </div>
-            <button onClick={sendMessage}>{t("making")}</button>
-          </div>
+          </Fade>
         </div>
         <Footer />
       </div>
